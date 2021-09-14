a0ae5abfb1428dc60b1aa273d0cf7de7a2037cf3
Pavel Yurchanka <pavel.yurchanka@oracle.com>
2021-09-14 19:49:31 +0300

    ADD: install additional packages

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,31 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({path: path.resolve(__dirname, '.env')});
 import express, { Application } from 'express';
 import mongoose from 'mongoose';
 import session from 'express-session';
 import MongoDBStore from 'connect-mongodb-session';
+import cors from 'cors';
+import hpp from 'hpp';
+import helmet from 'helmet';
 import { authRoutes, homeRoutes, profileRoutes, quizesRoutes } from './routes/routes';
-import { options } from './constants/options';
 import { routes } from './constants/routes';
 
 const app: Application = express();
 const MongoDBStoreSessioned = MongoDBStore(session);
 
 const store = new MongoDBStoreSessioned({
-  uri: options.MONGODB_URI,
+  uri: process.env.MONGODB_URI as string,
   collection: 'sessions'
 })
 
+app.use(helmet());
+app.use(cors());
+app.use(hpp());
 app.use(express.json());
 
 app.use(session({
-  secret: options.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET as string,
   resave: false,
   saveUninitialized: false,
   store
@@ -30,12 +38,12 @@ app.use('/quizes', quizesRoutes);
 
 void async function() {
   try {
-    await mongoose.connect(options.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URI as string, {
     })
-    app.listen(options.PORT, () => {
-      console.log('Server is running on port ' + options.PORT)
+    app.listen(process.env.PORT, () => {
+      console.log('Server is running on port ' + process.env.PORT)
     })
   } catch (error) {
     console.error(error)
   }
-}()
+}();
