92518d7f415901cba3a1109af41070a1ba13eb7d
Pavel Yurchanka <pavel.yurchanka@oracle.com>
2021-10-07 18:48:42 +0300

    start configurate email sending

---
+++ src/emails/transportConfig.ts
@@ -0,0 +1,26 @@
+const {google} = require('googleapis');
+
+const oAuth2Client = new google.auth.OAuth2(process.env.OAUTH_CLIENT_ID, process.env.OAUTH_CLIENT_SECRET, process.env.OAUTH_REDIRECT_URI);
+oAuth2Client.setCredentials({refresh_token: process.env.OAUTH_REFRESH_TOKEN});
+
+let accessToken;
+
+void async function() {
+  try {
+    accessToken = await oAuth2Client.getAccessToken();
+  } catch (err) {
+    console.error(err);
+  }
+}()
+
+export const oAuth2Config = {
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.OAUTH_EMAIL_FROM,
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    accessToken
+  }
+};
